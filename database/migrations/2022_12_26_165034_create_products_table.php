cd2e90882ffe70b4ca0a6e40a59583f6a205ea3b
Shah Alam <shahalam141072@gmail.com>
2022-12-26 23:01:44 +0600

    product migration model and controller created

---
+++ database/migrations/2022_12_26_165034_create_products_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('title');
+            $table->string('slug');
+            $table->string('description');
+            $table->longText('short_description')->nullable();
+            $table->longText('additional_info')->nullable();
+            $table->string('image');
+            $table->decimal('price');
+            $table->decimal('sale_price');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+};
