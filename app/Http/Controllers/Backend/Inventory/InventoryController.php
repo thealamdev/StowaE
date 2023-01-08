731528ca8bc03d5fbe09c632c875fd3ea5568837
Shah Alam <shahalam141072@gmail.com>
2023-01-08 16:14:01 +0600

    inventory index page is done

--- app/Http/Controllers/Backend/Inventory/InventoryController.php
+++ app/Http/Controllers/Backend/Inventory/InventoryController.php
@@ -19,6 +19,7 @@ class InventoryController extends Controller
     public function index()
     {
         //
+        return view('backend.inventory.index');
     }
 
     /**
---
+++ resources/views/backend/inventory/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.backendapp')
+@section('title','Inventory')
+@section('backendContent')
+<div class="row mt-3">
+    <div class="col-lg-12">
+        <div class="card">
+            <div class="card-body">
+                <div class="row">
+                    <div class="col-lg-12">
+                         <table class="table table-striped mb-0 thead-border-top-0">
+                            <thead>
+                                <th>Product id</th>
+                                <th>Color id</th>
+                                <th>Size id</th>
+
+                            </thead>
+                            <tr>
+                                <td>01</td>
+                                <td>02</td>
+                                <td>03</td>
+                            </tr>
+                         </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
