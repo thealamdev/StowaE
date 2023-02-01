a8822faa05d20f3349c500e4a67c91e556ed7fa4
Shah Alam <shahalam141072@gmail.com>
2023-02-02 03:09:46 +0600

    coupons model , migrations and controller created

---
+++ database/migrations/2023_02_01_210250_create_coupons_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('coupons', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->integer('amount');
+            $table->integer('applicable_amount');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('coupons');
+    }
+};
