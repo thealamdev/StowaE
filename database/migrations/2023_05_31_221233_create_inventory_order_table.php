d618b85403ccb308b8afd57130a835af872d5a9e
Shah Alam <shahalam141072@gmail.com>
2023-06-16 23:27:17 +0600

    done

--- database/migrations/2023_05_31_221233_create_inventory_order_table.php
+++ database/migrations/2023_05_31_221233_create_inventory_order_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('inventory_order', function (Blueprint $table) {
+        Schema::create('inventory_orders', function (Blueprint $table) {
             $table->id();
             $table->foreignId('inventory_id')->constrained();
             $table->foreignId('order_id');
